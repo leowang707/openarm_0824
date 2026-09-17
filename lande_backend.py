53676c419fd346775086f2c28d1f3bcb0c89ae99
leowang707 <leowang707@gmail.com>
2026-09-17 10:14:58 +0800

    cleanup: remove legacy paths and consolidate documentation

--- lande_backend.py
+++
@@ -1,5 +0,0 @@
-"""Backward-compatible import for the PA043 backend."""
-
-from motors.lande import LandeBackend, MODE_KEYS, MODE_KEYS_REVERSE
-
-__all__ = ["LandeBackend", "MODE_KEYS", "MODE_KEYS_REVERSE"]
